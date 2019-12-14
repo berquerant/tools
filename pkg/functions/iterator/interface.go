3be1b3a21df9e7539c99eb325bafee08463783d6
berquerant <sin@sin-pro.local>
2019-12-14 16:00:04 +0900

    Separate file

--- pkg/functions/iterator/interface.go
+++ pkg/functions/iterator/interface.go
@@ -31,73 +31,7 @@ type (
 	funcIterator struct {
 		f Func
 	}
-
-	// KV is a cell to contain element from iterator made from map
-	KV interface {
-		K() interface{}
-		V() interface{}
-	}
-	kv struct {
-		k, v interface{}
-	}
-	// IE is a cell to contain element sent from  the channel that made from iterator
-	IE interface {
-		I() interface{}
-		E() error
-	}
-	ie struct {
-		i interface{}
-		e error
-	}
 )
-
-func (s *kv) K() interface{} { return s.k }
-func (s *kv) V() interface{} { return s.v }
-
-func (s *ie) I() interface{} { return s.i }
-func (s *ie) E() error       { return s.e }
-
-// ToChan converts iterator into channel.
-// The channel is closed when iterator reached the end or some error
-func ToChan(iter Iterator) (<-chan IE, errors.Error) {
-	ch := make(chan IE)
-	go func() {
-		for {
-			x, err := iter.Next()
-			if err == EOI {
-				close(ch)
-				return
-			}
-			if err != nil {
-				ch <- &ie{e: err}
-				close(ch)
-				return
-			}
-			ch <- &ie{i: x}
-		}
-	}()
-	return ch, nil
-}
-
-// ToSlice convertes iterator into slice
-func ToSlice(iter Iterator) ([]interface{}, errors.Error) {
-	ret := []interface{}{}
-	for {
-		x, err := iter.Next()
-		if err == EOI {
-			return ret, nil
-		}
-		if err != nil {
-			return nil, errors.NewError().SetCode(errors.Iterator).SetError(err)
-		}
-		ret = append(ret, x)
-	}
-}
-
-// ToFunc converts iterator into function
-func ToFunc(iter Iterator) (Func, errors.Error) {
-	return Func(iter.Next), nil
-}
 
 func MustNew(v interface{}) Iterator {
 	iter, err := New(v)
