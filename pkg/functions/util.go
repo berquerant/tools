3ef1fc0f9a771b7eb7b477e7a769eb9cdd8c960a
berquerant <sin@sin-pro.local>
2019-12-15 23:43:04 +0900

    Add stream builder

---
+++ pkg/functions/util.go
@@ -0,0 +1,169 @@
+package functions
+
+import "tools/pkg/functions/fold"
+
+type (
+	// Script can be converted into a stream
+	Script interface {
+		// Type is type of stream
+		Type() ScriptType
+		// Instance is some function
+		Instance() interface{}
+		// Option is stream option
+		Option(int) interface{}
+		NumOption() int
+	}
+
+	script struct {
+		T ScriptType
+		I interface{}
+		O []interface{}
+	}
+
+	// ScriptBuilder builds stream from scripts
+	ScriptBuilder interface {
+		// Type set type of script
+		Type(ScriptType) ScriptBuilder
+		// Instance set instance of script
+		Instance(interface{}) ScriptBuilder
+		// Option append option of script
+		Option(interface{}) ScriptBuilder
+		Build() Script
+	}
+
+	scriptBuilder struct {
+		sc *script
+	}
+)
+
+func (s *script) Type() ScriptType         { return s.T }
+func (s *script) Instance() interface{}    { return s.I }
+func (s *script) Option(i int) interface{} { return s.O[i] }
+func (s *script) NumOption() int           { return len(s.O) }
+
+func NewScriptBuilder() ScriptBuilder {
+	return &scriptBuilder{
+		sc: &script{
+			O: []interface{}{},
+		},
+	}
+}
+
+func (s *scriptBuilder) Type(st ScriptType) ScriptBuilder {
+	s.sc.T = st
+	return s
+}
+
+func (s *scriptBuilder) Instance(f interface{}) ScriptBuilder {
+	s.sc.I = f
+	return s
+}
+
+func (s *scriptBuilder) Option(v interface{}) ScriptBuilder {
+	s.sc.O = append(s.sc.O, v)
+	return s
+}
+
+func (s *scriptBuilder) Build() Script {
+	return s.sc
+}
+
+func NewStreamFromScripts(in Stream, ss []Script) Stream {
+	s := NewStreamBuilder(in)
+	for _, x := range ss {
+		s.Append(x)
+	}
+	return s.Build()
+}
+
+//go:generate stringer -type=ScriptType -output generated.scripttype_string.go
+type ScriptType int
+
+const (
+	UnknownScriptType ScriptType = iota
+	// MapScriptType for Map
+	MapScriptType
+	// FilterScriptType for Filter
+	FilterScriptType
+	// FoldScriptType for Fold
+	FoldScriptType
+	// SortScriptType for Sort
+	SortScriptType
+	// FlatScriptType for Flat
+	FlatScriptType
+	// LiftScriptType for Lift
+	LiftScriptType
+)
+
+type (
+	// StreamBuilder builds stream from scripts
+	StreamBuilder interface {
+		Append(Script) StreamBuilder
+		Build() Stream
+	}
+
+	streamBuilder struct {
+		st Stream
+	}
+)
+
+func NewStreamBuilder(in Stream) StreamBuilder {
+	return &streamBuilder{
+		st: in,
+	}
+}
+
+func (s *streamBuilder) Append(x Script) StreamBuilder {
+	s.st = func() func(Script) Stream {
+		switch x.Type() {
+		case MapScriptType:
+			return s.appendMap
+		case FilterScriptType:
+			return s.appendFilter
+		case FoldScriptType:
+			return s.appendFold
+		case SortScriptType:
+			return s.appendSort
+		case FlatScriptType:
+			return s.appendFlat
+		case LiftScriptType:
+			return s.appendLift
+		}
+		return func(Script) Stream { return s.st }
+	}()(x)
+	return s
+}
+
+func (s *streamBuilder) appendMap(x Script) Stream {
+	return s.st.Map(x.Instance())
+}
+
+func (s *streamBuilder) appendFilter(x Script) Stream {
+	return s.st.Filter(x.Instance())
+}
+
+func (s *streamBuilder) appendFold(x Script) Stream {
+	opts := []fold.FoldOption{}
+	for i := 0; i < x.NumOption(); i++ {
+		if p, ok := x.Option(i).(fold.FoldOption); ok {
+			opts = append(opts, p)
+		}
+	}
+	return s.st.Fold(x.Instance(), opts...)
+}
+
+func (s *streamBuilder) appendSort(x Script) Stream {
+	return s.st.Sort(x.Instance())
+}
+
+func (s *streamBuilder) appendFlat(x Script) Stream {
+	return s.st.Flat()
+}
+
+func (s *streamBuilder) appendLift(x Script) Stream {
+	return s.st.Lift()
+}
+
+func (s *streamBuilder) Build() Stream {
+	return s.st
+}
---
+++ pkg/functions/util_test.go
@@ -0,0 +1,164 @@
+package functions_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"tools/pkg/functions"
+	"tools/pkg/functions/fold"
+	"tools/pkg/functions/iterator"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+type (
+	row struct {
+		T functions.ScriptType
+		I interface{}
+		O []interface{}
+	}
+)
+
+func (s *row) Script() functions.Script {
+	sb := functions.NewScriptBuilder()
+	sb.Type(s.T)
+	if s.I != nil {
+		sb.Instance(s.I)
+	}
+	for _, x := range s.O {
+		sb.Option(x)
+	}
+	return sb.Build()
+}
+
+type (
+	streamBuilderTestcase struct {
+		Comment string
+		Rows    []row
+		Data    interface{}
+		Result  []interface{}
+	}
+)
+
+func (s *streamBuilderTestcase) Test(t *testing.T) {
+	sb := functions.NewStreamBuilder(functions.NewStream(iterator.MustNew(s.Data)))
+	for _, r := range s.Rows {
+		sb.Append(r.Script())
+	}
+	st := sb.Build()
+	actual, err := iterator.ToSlice(st)
+	if err != nil {
+		t.Errorf("to slice: %v", err)
+	}
+	if err := st.Err(); err != nil {
+		t.Errorf("stream error: %v", err)
+	}
+	if !cmp.Equal(actual, s.Result) {
+		t.Errorf("not expected result:\n  actual(%#v)\nexpected(%#v)", actual, s.Result)
+	}
+}
+
+func TestStreamBuilder(t *testing.T) {
+	testcases := []*streamBuilderTestcase{
+		&streamBuilderTestcase{
+			// awk '{d[toupper($0)]++} END {for (k in d) if (d[k] > 1) print k, d[k]}' | sort -nk 2 with handicaps
+			Comment: "stat-with-handicaps",
+			Data: []string{
+				"Romania",
+				"Norway",
+				"Romania",
+				"England",
+				"China",
+				"Greece",
+				"Canada",
+				"Slovenia",
+				"Nepal",
+				"Romania",
+				"Slovakia",
+				"Iran",
+				"Greece",
+				"Nepal",
+				"Slovakia",
+				"Turkey",
+				"Romania",
+				"Turkey",
+				"United States",
+				"Tunisia",
+				"New Zealand",
+				"Romania",
+				"Turkey",
+				"Nepal",
+				"Kyrgyz Republic",
+				"Romania",
+				"Slovakia",
+				"Turkey",
+				"England",
+				"United States",
+			},
+			Result: []interface{}{
+				"UNITED STATES 2",
+				"GREECE 2",
+				"NEPAL 3",
+				"TURKEY 4",
+				"SLOVAKIA 5",
+				"ROMANIA 6",
+				"ENGLAND 12",
+			},
+			Rows: []row{
+				{
+					T: functions.MapScriptType,
+					I: func(x string) string {
+						return strings.ToUpper(x)
+					},
+				},
+				{
+					T: functions.FoldScriptType,
+					I: func(x string, d map[string]int) map[string]int {
+						d[x]++
+						return d
+					},
+					O: []interface{}{
+						fold.WithFoldType(fold.FoldTypeR),
+						fold.WithInitialValue(map[string]int{
+							"ENGLAND":  10,
+							"SLOVAKIA": 2,
+						}),
+					},
+				},
+				{
+					T: functions.FlatScriptType,
+				},
+				{
+					T: functions.FilterScriptType,
+					I: func(x iterator.KV) bool {
+						return x.V().(int) > 1
+					},
+				},
+				{
+					T: functions.SortScriptType,
+					I: func(x, y iterator.KV) bool {
+						return x.K().(string) > y.K().(string)
+					},
+				},
+				{
+					T: functions.SortScriptType,
+					I: func(x, y iterator.KV) bool {
+						return x.V().(int) < y.V().(int)
+					},
+				},
+				{
+					T: functions.MapScriptType,
+					I: func(x iterator.KV) string {
+						return fmt.Sprintf("%v %v", x.K(), x.V())
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range testcases {
+		t.Run(tt.Comment, func(t *testing.T) {
+			tt.Test(t)
+		})
+	}
+}
